9a4d8826a51ff57ff7eec91cfa0b3ec59ffa3ebf
Lifanna3 <solano.lifan2@bk.ru>
2021-03-23 17:36:36 +0600

    fixed string interpolation

--- background.js
+++ background.js
@@ -3,15 +3,16 @@
 
 // требуется убирать в слове ударения.
 
-const url=chrome.runtime.getURL('popup/wordstudy.html?lemma=λήμμα&form=φόρμα');	
+// const url=chrome.runtime.getURL('popup/wordstudy.html?lemma=λήμμα&form=φόρμα');	
+const url=chrome.runtime.getURL('popup/meanings.html?lemma=λήμμα&form=φόρμα');	
 chrome.tabs.create({url: url})	
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
+	
 	switch(request.todo)
 	{
 		case 'meanings':
-			//alert(request.word)
+			alert(request.word)
 			break
 
 		case 'wordstudy':
@@ -24,7 +25,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			//open modern Greek
 			var word=request.word
 			if(word.length>=5)
-			word=word.substr(0,word.length*2/3).normalize();  
+			word=word.substr(0,word.length*2/3).normalize();
 
 			var monotonic = word.normalize('NFD').replace(/[\u0300\u0342]/g, String.fromCharCode(0x0301)).
 						replace(/[\u0302-\u0307\u0309-\u0343\u0345-\u036f]/g, '').normalize();
@@ -35,7 +36,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			var url =	greeklangUrl
 			chrome.tabs.create({url: url});
 
-			//WordView(request.word);
+			WordStudy(request.word);
 
 	}
 	
